6fd1df40543df852134c5c10f9b839d9bb035237
jonny desktop <jon@uslms.com>
2023-10-30 10:28:57 -0600

    I made the array of boolean values into an adjacency list with listnode and graph classes

--- src/app/maze-box/maze-box.component.ts
+++ src/app/maze-box/maze-box.component.ts
@@ -1,4 +1,55 @@
-import { Component, OnInit, Output, EventEmitter, HostListener } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  HostListener,
+} from '@angular/core';
+
+class ListNode {
+  value: number;
+  neighbors: ListNode[];
+
+  constructor(value: number) {
+    this.value = value;
+    this.neighbors = [];
+  }
+
+  addNeighbor(node: ListNode): void {
+    this.neighbors.push(node);
+  }
+}
+
+class Graph {
+  nodes: ListNode[];
+
+  constructor() {
+    this.nodes = [];
+  }
+
+  addNode(value: number): ListNode {
+    const newNode = new ListNode(value);
+    this.nodes.push(newNode);
+    return newNode;
+  }
+
+  findNode(value: number): ListNode | undefined {
+    return this.nodes.find((node) => node.value === value);
+  }
+
+  addEdge(value1: number, value2: number): void {
+    const node1 = this.findNode(value1);
+    const node2 = this.findNode(value2);
+
+    if (!node1 || !node2) {
+      console.error('One or both nodes not found.');
+      return;
+    }
+
+    node1.addNeighbor(node2);
+    node2.addNeighbor(node1);
+  }
+}
 
 @Component({
   selector: 'mazeBox',
@@ -13,12 +64,32 @@ export class MazeBoxComponent implements OnInit {
 
   isMouseDown: boolean = false;
 
+  mazeGraph = new Graph
+
   ngOnInit() {
     for (let i = 0; i <= 11; i++) {
       this.yIds.push(i);
       this.xIds.push(i);
     }
+
+    for(let i=0; i<144; i++){
+      this.mazeGraph.addNode(i)
+    }
+
+    for(let i=0; i<this.mazeGraph.nodes.length; i++){
+      if((this.mazeGraph.nodes[i].value + 1) % 12 !== 0){
+        this.mazeGraph.addEdge(i, i + 1)
+      }
+      if(this.mazeGraph.nodes[i].value + 12 < 144){
+        this.mazeGraph.addEdge(i, i + 12)
+      }
+    }
+    console.log(this.mazeGraph.nodes)
   }
+
+
+
+
 
   mazeArray = Array(12)
     .fill(null)
@@ -46,12 +117,26 @@ export class MazeBoxComponent implements OnInit {
 
   onSquareHover(y: number, x: number) {
     if (this.isMouseDown) {
-      this.changeArrayHandler( y, x );
+      this.changeArrayHandler(y, x);
     }
   }
 
   changeArrayHandler(y: number, x: number) {
     this.mazeArray[y][x] = !this.mazeArray[y][x];
     this.mazeArrayChange.emit(this.mazeArray);
+  }
+
+
+  //
+  //
+  //
+
+
+  testButtonHandler(){
+    console.log(this.mazeGraph)
+  }
+
+  nodeClickHandler(node: ListNode){
+    console.log(node)
   }
 }
