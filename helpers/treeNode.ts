d78bccc7fa7c189db320879c55a0cf94383f3169
Carter <carreejoh4637@gmail.com>
2023-10-31 20:25:18 -0600

    Added animations to tree, need to make scalable code

--- helpers/treeNode.ts
+++ helpers/treeNode.ts
@@ -2,8 +2,26 @@ export class TreeNode {
     key: number;
     left: TreeNode | null = null;
     right: TreeNode | null = null;
+    isActive: boolean = false
 
     constructor(key: number) {
         this.key = key;
+    }
+}
+
+export class TreeComponent {
+    rootNode: TreeNode;
+
+    constructor() {
+        this.rootNode = new TreeNode(20);
+
+        this.addChildNodes(this.rootNode, 10, 30);
+        this.addChildNodes(this.rootNode.left!, 5, 15);
+        this.addChildNodes(this.rootNode.right!, 25, 35);
+    }
+
+    addChildNodes(node: TreeNode, leftKey: number, rightKey: number): void {
+        node.left = new TreeNode(leftKey);
+        node.right = new TreeNode(rightKey);
     }
 }
